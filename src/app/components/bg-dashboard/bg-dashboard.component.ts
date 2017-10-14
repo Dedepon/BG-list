daced67d06b4ac690dc3744ff9e703c6b7cfc12b
Denis Bourguignon <bourguignon.denis.2012@gmail.com>
2017-10-14 17:07:01 +0200

    Dashboard creation Creation of the base structure of dashboard

--- src/app/components/bg-dashboard/bg-dashboard.component.ts
+++ src/app/components/bg-dashboard/bg-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { LoggerService } from '../../services/logger/logger.service';
 
 @Component({
   selector: 'bgl-bg-dashboard',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bg-dashboard.component.css']
 })
 export class BgDashboardComponent implements OnInit {
+  colId: String;
+  bgId: String;
+  listBgId: String;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  fetchCollection(): void {
+    this.router.navigate(['/collection'], { queryParams: { colId: this.colId } })
+      .catch(LoggerService.error);
   }
 
+  fetchFullTree(): void {
+    this.router.navigate(['/tree'], { queryParams: { bgId: this.bgId } })
+      .catch(LoggerService.error);
+  }
+
+  fetchList(): void {
+    if (this.listBgId.indexOf(',') === -1) {
+      this.router.navigate(['/details', this.listBgId])
+        .catch(LoggerService.error);
+    } else {
+      this.router.navigate(['/list'], { queryParams: { listBgId: this.listBgId } })
+        .catch(LoggerService.error);
+    }
+  }
 }
