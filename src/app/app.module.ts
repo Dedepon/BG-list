a434b9d1efa2acbbb88d3f443f9c3c9ccd16780c
Denis Bourguignon <bourguignon.denis.2012@gmail.com>
2017-10-14 17:08:19 +0200

    FormsModule import Import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -24,7 +25,8 @@ import { BgDashboardComponent } from './components/bg-dashboard/bg-dashboard.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
