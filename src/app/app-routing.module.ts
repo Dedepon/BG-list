b726725b0d97ce8714ecf85a096a536d26fa6b1e
Denis Bourguignon <bourguignon.denis.2012@gmail.com>
2017-10-14 17:07:52 +0200

    Routing adaptation Create route to "tree" Adapt details route parameter name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { BgDashboardComponent } from './components/bg-dashboard/bg-dashboard.com
 import { BgListComponent } from './components/bg-list/bg-list.component';
 import { BgDetailComponent } from './components/bg-detail/bg-detail.component';
 import { BgCollectionComponent } from './components/bg-collection/bg-collection.component';
+import { BgTreeComponent } from './components/bg-tree/bg-tree.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: BgDashboardComponent },
   { path: 'list', component: BgListComponent },
   { path: 'collection', component: BgCollectionComponent },
-  { path: 'details/:id', component: BgDetailComponent }
+  { path: 'tree', component: BgTreeComponent },
+  { path: 'details/:bgid', component: BgDetailComponent }
 ];
 
 @NgModule({
