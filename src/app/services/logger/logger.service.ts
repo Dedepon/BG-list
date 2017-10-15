32a6fe6d2c2b291f85900cfa8ce267c3f2a6d551
Denis Bourguignon <bourguignon.denis.2012@gmail.com>
2017-10-15 23:44:49 +0200

    Logger service adaptation Creation of pattern for data logging - Date - Message type (INFO-ERROR-WARNING)

--- src/app/services/logger/logger.service.ts
+++ src/app/services/logger/logger.service.ts
@@ -6,14 +6,14 @@ export class LoggerService {
   constructor() { }
 
   static error(e: string): void {
-    console.error(e);
+    console.error(new Date().toUTCString() + ' [ERROR] : ' + e);
   }
 
   static log(l: string): void {
-    console.log(l);
+    console.log(new Date().toUTCString() + ' [INFO]: ' + l);
   }
 
   static warn(w: string): void {
-    console.warn(w);
+    console.warn(new Date().toUTCString() + ' [WARNING] : ' + w);
   }
 }
