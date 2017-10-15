1a104e4aa05f7798dbb5c74c0d35b7e81843b46e
Denis Bourguignon <bourguignon.denis.2012@gmail.com>
2017-10-15 23:48:54 +0200

    Creation of BgService - retrieve bg based on id - retrieve bg list base on id list - retrieve expansion list based on a Bg object - retrieve tree based on root board game - creates Bg tuples from map for easier treatment in component

--- src/app/services/bg/bg.service.ts
+++ src/app/services/bg/bg.service.ts
@@ -1,8 +1,90 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/catch';
+
+import { Bg } from '../../model/bg';
+import { LoggerService } from '../logger/logger.service';
+import { BgParserService } from '../bg-parser/bg-parser.service';
+
+const url = 'https://www.boardgamegeek.com/xmlapi2/thing/?id=';
 
 @Injectable()
 export class BgService {
+  private bgList: Map<string, Bg> = new Map();
+  constructor(private http: Http,
+              private bgParser: BgParserService) { }
 
-  constructor() { }
+  getBgInfos(id: string): Promise<Bg> {
+    if (this.bgList[id]) { return new Promise<Bg> (resolve => resolve(this.bgList[id])); }
+
+    return this.http.get(url + id)
+      .toPromise()
+      .then(response => {
+        this.bgList[id] = this.bgParser.parseBgData(response.text())[0];
+        return this.bgList[id];
+      })
+      .catch(val => {
+        LoggerService.error(val);
+      });
+  }
+
+  getBgTree(rootId: string): Promise<[string, Bg][]> {
+    return this.getBgInfos(rootId)
+      .then(bg => {
+        return this.getExpansionDetails(bg)
+          .then(() => {
+            return new Promise(resolve => resolve(this.mapToTupleArray(rootId)));
+          });
+      });
+  }
+
+  getBgListInfos(id: string): Promise<Map<string, Bg>> {
+    if (id.indexOf(',') === -1) { return this.getBgInfos(id).then(bg => {
+        return new Map([[bg.id, bg]]);
+      });
+    }
+
+    // TODO parse which id is already in memory !
+
+    return this.http.get(url + id)
+      .toPromise()
+      .then(response => {
+        const expList: Bg[] = this.bgParser.parseBgData(response.text());
+        for (let j = 0; j < expList.length; j++) {
+          this.bgList[expList[j].id] = expList[j];
+        }
+        return this.bgList; // todo change the return for only the bg from the list
+      })
+      .catch(val => {
+        LoggerService.error(val);
+      });
+  }
+
+  getExpansionDetails(bg: Bg): Promise<Map<string, Bg>> {
+    let expIds = '';
+    for (let i = 0; i < bg.expansionsIDs.length; i++) {
+      if (expIds !== '') { expIds = expIds + ','; }
+      expIds = expIds + bg.expansionsIDs[i];
+    }
+    if (expIds === '') { return new Promise(resolve => resolve([bg])); }
+    return this.getBgListInfos(expIds)
+      .then(list => {
+        return list; // todo check list of further exp
+      });
+  }
+
+  private mapToTupleArray(root: string): [string, Bg][] {
+    const rootBg = this.bgList[root];
+    let list: [string, Bg][] = [[root, rootBg]];
+    if (rootBg.expansionsIDs.length !== 0) {
+      for (let i = 0; i < rootBg.expansionsIDs.length; i++) {
+        const expList: [string, Bg][] = this.mapToTupleArray(rootBg.expansionsIDs[i]);
+        list = list.concat(expList);
+      }
+    }
+    return list;
+  }
 
 }
