fd3a7cadcb6d18d2d0cdc93bbda0202a52aef2ba
dbourguignon <denis.bourguignon@soprabanking.com>
2017-10-16 16:10:15 +0200

    Update xml to Bg parsing Handle different cases and fill all info in returned Bg object

--- src/app/services/bg-parser/bg-parser.service.ts
+++ src/app/services/bg-parser/bg-parser.service.ts
@@ -24,44 +24,122 @@ export class BgParserService {
   }
 
   private static createBg(data: HTMLElement): Bg {
-    const id: string = data.getAttribute('id');
-    const name: string = data.getElementsByTagName('name')[0].getAttribute('value');
-    const links: any = data.getElementsByTagName('link');
-    const expansions: string[] = [];
+    // Initialize the new Bg
+    const bg: Bg = new Bg();
 
+    // Set the id
+    bg.id = data.getAttribute('id');
+
+    // Set the name
+    let element: Element = data.getElementsByTagName('name').item(0);
+    if (element !== null) {
+      bg.name = element.getAttribute('value');
+    }
+
+    // Set the url
+    bg.url = '';
+
+    // Set the thumbnail url
+    element = data.getElementsByTagName('thumbnail').item(0);
+    if (element !== null) {
+      bg.thumbnail = element.innerHTML;
+    }
+
+    // Set the image url
+    element = data.getElementsByTagName('image').item(0);
+    if (element !== null) {
+      bg.image = element.innerHTML;
+    }
+
+    // Set min player count
+      element = data.getElementsByTagName('minplayers').item(0);
+    if (element !== null) {
+      bg.minPlayers = +element.getAttribute('value');
+    }
+
+    // Set max player count
+    element = data.getElementsByTagName('maxplayers').item(0);
+    if (element !== null) {
+      bg.maxPlayers = +element.getAttribute('value');
+    }
+
+    // Set the description
+    element = data.getElementsByTagName('description').item(0);
+    if (element !== null) {
+      bg.description = element.innerHTML.replace(/&amp;#10;/g, '\n');
+    }
+
+    // Set the play time
+    element = data.getElementsByTagName('playingtime').item(0);
+    if (element !== null) {
+      bg.playTime = +element.getAttribute('value');
+    }
+
+    // Set the minimum age
+    element = data.getElementsByTagName('minage').item(0);
+    if (element !== null) {
+      bg.minAge = +element.getAttribute('value');
+    }
+
+    // Set the data lists
+    const links: any = data.getElementsByTagName('link');
+    bg.expansionsIDs = [];
+    bg.expansionOfId = [];
+    bg.category = [];
+    bg.mechanic = [];
+    bg.family = [];
+    bg.publisher = [];
+    bg.designer = [];
+    bg.artist = [];
     for (let i = 0; i < links.length; i++) {
-      if (links[i].getAttribute('type') === 'boardgameexpansion' && !links[i].getAttribute('inbound')) {
-        expansions.push(links[i].getAttribute('id'));
+      element = links.item(i);
+      switch (element.getAttribute('type')) {
+        case 'boardgameexpansion' :
+          if (!element.getAttribute('inbound')) {
+            bg.expansionsIDs.push(element.getAttribute('id'));
+          } else {
+            bg.expansionOfId.push(element.getAttribute('id'));
+          }
+          break;
+        case 'boardgamecategory' :
+          bg.category.push(element.getAttribute('id'));
+          break;
+        case 'boardgamemechanic ' :
+          bg.mechanic.push(element.getAttribute('id'));
+          break;
+        case 'boardgamefamily' :
+          bg.family.push(element.getAttribute('id'));
+          break;
+        case 'boardgamedesigner' :
+          bg.designer.push(element.getAttribute('id'));
+          break;
+        case 'boardgameartist' :
+          bg.artist.push(element.getAttribute('id'));
+          break;
+        case 'boardgamepublisher' :
+          bg.publisher.push(element.getAttribute('id'));
+          break;
       }
     }
 
-    return {
-      id: id,
-      name: name,
-      url: '',
+    // Set the rating and rank
+    element = data.getElementsByTagName('statistics').item(0);
+    if (element !== null) {
+      element = element.getElementsByTagName('ratings').item(0);
+      if (element !== null) {
+        if (element.getElementsByTagName('average').item(0) !== null) {
+          bg.rating = +element.getElementsByTagName('average').item(0).getAttribute('value');
+        }
+        element = element.getElementsByTagName('average').item(0);
+        if (element !== null) {
+          element = element.getElementsByTagName('rank').item(0);
+          if (element !== null && element.getAttribute('name') === 'boardgame') {
+            bg.rank = +element.getAttribute('value');
+          }
+        }
+      }
+    }
 
-      image: '',
-      thumbnail: '',
-
-      minPlayers: 2,
-      maxPlayers: 4,
-      description: 'HAHAAAA ! description !',
-      playTime: 120,
-      minAge: 14,
-
-      category: [],
-      mechanic: [],
-      family: [],
-
-      rating: 5,
-
-      publisher: [],
-      designer: [],
-      author: [],
-      artist: [],
-
-      expansionsIDs: expansions,
-      expansionOfId: ''
-    };
+    return bg;
   }
 }
